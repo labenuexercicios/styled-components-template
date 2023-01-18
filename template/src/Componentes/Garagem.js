eb1d4805a85dbb9fd49c04d14ca5dc992be5bbbd
Murilo De Sousa Gonçalves <40380133+Muri01@users.noreply.github.com>
2023-01-18 14:31:01 -0300

    Update Garagem.js
    incluir valores na prop flex

--- template/src/Componentes/Garagem.js
+++ template/src/Componentes/Garagem.js
@@ -6,10 +6,10 @@ export function Garagem(props) {
       <h1>Garagem da {props.nome}</h1>
       <button onClick={props.mensagemAprentacao}>Mensagem</button>
 
-      <Carro adicionadoPor={props.nome} cor={"Vermelho"} ano={2022} flex />
-      <Carro adicionadoPor={props.nome} cor={"Verde"} ano={2020} flex />
-      <Carro adicionadoPor={props.nome} cor={"Amarelo"} ano={2009} />
-      <Carro adicionadoPor={props.nome} cor={"Rosa"} ano={2022} />
+      <Carro adicionadoPor={props.nome} cor={"Vermelho"} ano={2022} flex={"true"} />
+      <Carro adicionadoPor={props.nome} cor={"Verde"} ano={2020} flex={"false"}  />
+      <Carro adicionadoPor={props.nome} cor={"Amarelo"} ano={2009} flex={"false"} />
+      <Carro adicionadoPor={props.nome} cor={"Rosa"} ano={2022} flex={"true"} />
     </div>
   );
 }
